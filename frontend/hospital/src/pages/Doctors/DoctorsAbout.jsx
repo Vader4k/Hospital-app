5becd8e464649063c99afc66bfdba4359df7627e
vader2k <76834964+vader2k@users.noreply.github.com>
2023-10-07 20:12:10 +0100

    added new list

--- frontend/hospital/src/pages/Doctors/DoctorsAbout.jsx
+++ frontend/hospital/src/pages/Doctors/DoctorsAbout.jsx
@@ -43,6 +43,20 @@ const DoctorsAbout = () => {
 
         <div className='mt-12'>
             <h3 className='text-[20px] leading-[30px] text-headingBg font-semibold'>Experience</h3>
+            <ul className='grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5'>
+                <li className='p-4 rounded bg-[#fff9ea]'>
+                    <span className='text-yellow text-[15px] leading-6 font-semibold'>
+                    {formatDate("07-04-2010")} - {formatDate("09-13-2014")}
+                    </span>
+                    <p className='text-[16px] leading-6 font-medium text-textcol'>
+                        Sr. Surgeon
+                    </p>
+
+                    <p className='text-[14px] leading-5 font-medium text-textcol'>
+                        New Apollo Hospital, New York.
+                    </p>
+                </li>
+            </ul>
         </div>
     </div>
   )
